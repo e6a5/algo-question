e451582dc68f459d98f2fd5906698ae6e6d97324
tranhiepqna <tranhiepqna@gmail.com>
2023-03-23 23:12:34 +0700

    searching: binary search

---
+++ searching/binary_search.go
@@ -0,0 +1,18 @@
+package main
+
+func BinarySearch(array []int, target int) int {
+	// Write your code here.
+	low := 0
+	high := len(array) - 1
+	for low <= high {
+		mid := (low + high) / 2
+		if array[mid] == target {
+			return mid
+		} else if array[mid] < target {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+	return -1
+}
