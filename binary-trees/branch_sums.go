6eec63850d80fdaaadeba27f9321191e4e6d53e1
tranhiepqna <tranhiepqna@gmail.com>
2023-03-26 23:41:20 +0700

    binary-trees: branch sums

---
+++ binary-trees/branch_sums.go
@@ -0,0 +1,25 @@
+package main
+
+type BinaryTree struct {
+	Value int
+	Left  *BinaryTree
+	Right *BinaryTree
+}
+
+func BranchSums(root *BinaryTree) []int {
+	// Write your code here.
+	if root == nil {
+		return []int{}
+	}
+	if root.Left == nil && root.Right == nil {
+		return []int{root.Value}
+	}
+	sums := make([]int, 0)
+	for _, sum := range BranchSums(root.Left) {
+		sums = append(sums, sum+root.Value)
+	}
+	for _, sum := range BranchSums(root.Right) {
+		sums = append(sums, sum+root.Value)
+	}
+	return sums
+}
